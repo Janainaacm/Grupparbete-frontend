e3b5b4a43fd8289d8f80d94ba35a1f244c10e35c
Maksim <Mp-104@hotmail.com>
2024-02-26 13:17:35 +0100

    test commit

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// test commitdd
 function App() {
   const [count, setCount] = useState(0)
 
@@ -26,7 +27,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn more test
       </p>
     </>
   )
