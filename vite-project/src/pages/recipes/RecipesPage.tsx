55b9f1714e4385becb1856bdf6fc234604750912
Janainaacm <janaina.maneiro@gmail.com>
2024-03-13 11:33:50 +0100

    starting on RecipesPage

--- vite-project/src/pages/recipes/RecipesPage.tsx
+++ vite-project/src/pages/recipes/RecipesPage.tsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import { useLocation } from "react-router-dom";
+import axios from "axios";
+
 
 
 const recipesPage = () => {
+    const URL = "https://sti-java-grupp5-wjfjet.reky.se/swagger";
+
     const { state } = useLocation();
+
+    const fetchAllRecipes = async () => {
+        // Kalla på min API GET /todos
+      };
+
     if (!state) return(
         //Visa alla recept
         console.log("hej")
     );
+
+
+
 
   return (
     <div>
