f90b9cf7ca6fc84c3812dda4ec5e11017083bfc7
willyfall <williamnhc@icloud.com>
2024-03-14 14:10:54 +0100

    Added searchbar to navbar

--- vite-project/src/global_components/NavBar/NavBar.tsx
+++ vite-project/src/global_components/NavBar/NavBar.tsx
@@ -1,34 +1,37 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import SearchBarComponent from '../SearchBarComponent'; 
 
 function NavBar() {
-  return (
-    <nav style={{ backgroundColor: 'rgb(253, 245, 230)', padding: '10px 0', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)' }}>
-      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', maxWidth: '800px', margin: '0 auto' }}>
-        <div style={{ textAlign: 'left' }}>
-          <a href="#" style={{ display: 'flex', alignItems: 'center', textDecoration: 'none' }}>
-            <img src="" 
-              width="60" 
-              height="60" 
-              style={{ marginRight: '10px'}} 
-              alt=""
-            />
-            <div style={{ fontWeight: 'bold', fontSize: '1.5rem', color: 'rgb(210,105,30)' }}>Receptkungen.se</div>
-          </a>
-        </div>
-        <div style={{ textAlign: 'center' }}>
-          <ul style={{ listStyle: 'none', margin: 0, padding: 0, display: 'inline-block' }}>
-            <li style={{ display: 'inline-block', margin: '0 10px' }}>
-            <Link to="/Home" style={{ textDecoration: 'none', color: 'rgb(210,105,30)' }}>Home</Link>
-            </li>
+    const location = useLocation();
 
-              <li style={{ display: 'inline-block', margin: '0 10px' }}>
-              <Link to="/Recept" style={{ textDecoration: 'none', color: 'rgb(210,105,30)' }}>Recept</Link>
-              </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+    return (
+        <nav style={{ backgroundColor: 'rgb(253, 245, 230)', padding: '10px 0', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)' }}>
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', maxWidth: '800px', margin: '0 auto' }}>
+                <div style={{ textAlign: 'left' }}>
+                <Link to="/" style={{ display: 'flex', alignItems: 'center', textDecoration: 'none' }}>
+                  <img src="" width="60" height="60" style={{ marginRight: '10px' }} alt="" />
+                  <div style={{ fontWeight: 'bold', fontSize: '1.5rem', color: 'rgb(210,105,30)' }}>Receptkungen.se
+                  </div>
+                </Link>
+                </div>
+                <div style={{ textAlign: 'center' }}>
+                    <ul style={{ listStyle: 'none', margin: 0, padding: 0, display: 'inline-block' }}>
+                        <li style={{ display: 'inline-block', margin: '0 10px' }}>
+                            <Link to="/Home" style={{ textDecoration: 'none', color: 'rgb(210,105,30)' }}>Home</Link>
+                        </li>
+                        <li style={{ display: 'inline-block', margin: '0 10px' }}>
+                            <Link to="/Recept" style={{ textDecoration: 'none', color: 'rgb(210,105,30)' }}>Recept</Link>
+                        </li>
+                    </ul>
+                </div>
+                {location.pathname !== '/' ? (
+                    <div>
+                        <SearchBarComponent />
+                    </div>
+                ) :null}
+            </div>
+        </nav>
+    );
 }
 
-export default NavBar
+export default NavBar;
--- vite-project/src/pages/recipes/RecipePage.tsx
+++ vite-project/src/pages/recipes/RecipePage.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import NavBar from '../../global_components/NavBar/NavBar'
 
 const RecipePage = () => {
   return (
+    <>
+    <NavBar/>
     <div style={{backgroundColor: 'orange'}}>
       <p>RecipePage</p>
     </div>
+    </>
+    
   )
 }
 
