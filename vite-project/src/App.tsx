4b4e66b6561ce0363ba28aa01270de828ed94436
Nick <61873872+LedinN@users.noreply.github.com>
2024-03-14 10:06:17 +0100

    tested function, delete works

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css'
+import deleteRecipe from './api/deleteRecipe';
+import SearchBarComponent from './global_components/SearchBarComponent';
 
 
 function App() {
-
+deleteRecipe("65f2bd6139aa131cc702feba")
   const searchBar = SearchBarComponent
 
   return (
