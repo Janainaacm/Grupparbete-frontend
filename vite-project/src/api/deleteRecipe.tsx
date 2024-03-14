8a1b48b8af384a0419d231ffdd10701f861b72c2
Nick <61873872+LedinN@users.noreply.github.com>
2024-03-14 09:55:37 +0100

    delete function first draft

---
+++ vite-project/src/api/deleteRecipe.tsx
@@ -0,0 +1,17 @@
+import { API_URL } from "../config";
+import axios from "axios";
+
+
+async function deleteRecipe(recipeId:string) {
+
+    try {
+        const response = await axios.delete(`${API_URL}/recipes/${recipeId}`)
+    
+    if (response.status === 204){
+        console.log("Recipe deleted")
+    }
+    } catch (error) {
+    console.log('Error deleting recipe', error)
+}
+
+}export default deleteRecipe
