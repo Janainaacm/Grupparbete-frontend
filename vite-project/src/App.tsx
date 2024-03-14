c1edb12df2779b81d32b694ff01b6997cfd5881c
willyfall <williamnhc@icloud.com>
2024-03-14 12:33:38 +0100

    Added NavBar component

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,12 +1,18 @@
 import './App.css'
-
+import useGetAllRecipes from './api/getAllRecipes';
+import SearchBarComponent from './global_components/SearchBarComponent';
+import NavBar from './global_components/NavBar/NavBar';
 
 function App() {
 
   const searchBar = SearchBarComponent
+  const navBar=NavBar
 
   return (
-    searchBar()
+    <>
+    {navBar()}
+    {searchBar()}
+    </>
   );
   
 }
--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/home.tsx";
+import RecipePage from "./pages/recipes/RecipePage.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Recept" element={<RecipePage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
