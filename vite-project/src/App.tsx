e65bca68d51e386aabf7cce150b93b6459ed593f
Bojken07 <bojken.zaimi@stud.sti.se>
2024-03-14 13:12:21 +0100

    footer button om oss

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -2,14 +2,17 @@ import './App.css'
 import useGetAllRecipes from './api/getAllRecipes';
 import SearchBarComponent from './global_components/SearchBarComponent';
 import NavBar from './global_components/NavBar/NavBar';
+import Footer from './global_components/footer';
 
 function App() {
 
   const searchBar = SearchBarComponent
   const navBar=NavBar
+  const footer=Footer
 
   return (
     <>
+    {footer()}
     {navBar()}
     {searchBar()}
     </>
