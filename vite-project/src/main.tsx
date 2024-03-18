e8b66fb5fef3541933c9f7f4a46d1f9868ee8856
Janainaacm <janaina.maneiro@gmail.com>
2024-03-18 13:41:10 +0100

    Navigate with state

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -4,8 +4,10 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/homeTest.tsx";
-import RecipePage from "./pages/recipes/RecipePageTest.tsx";
+import RecipePage from "./pages/recipes/RecipePage.tsx";
 import AddRecipePage from "./pages/recipes/AddRecipePage.tsx";
+
+
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
@@ -16,6 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <Route path="/" element={<Home />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/Recept/:title" element={<RecipePage />} />
+        <Route path="/Recept" element={<RecipePage />} />
         <Route path="/AddRecept" element={<AddRecipePage/>} />
       </Routes>
     </BrowserRouter>
