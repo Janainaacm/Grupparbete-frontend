f2574b721c3060213fb5529022ebd57de78abb25
Bojken07 <bojken.zaimi@stud.sti.se>
2024-03-23 16:20:36 +0100

    add

--- vite-project/src/global_components/FilterFunktion.tsx
+++ vite-project/src/global_components/FilterFunktion.tsx
@@ -1,55 +1,40 @@
-import { useLocation, useNavigate } from 'react-router-dom';
-import { RecipeInterface } from '../Types';
-import axios from 'axios'
+import axios from 'axios';
+import React, { useState } from 'react';
 import { API_URL } from '../config';
-import { BiCategory } from 'react-icons/bi';
-import { useState } from 'react';
-import { JSX } from 'react/jsx-runtime';
+import { RecipeInterface } from '../Types';
+import { JSX } from 'react/jsx-dev-runtime';
+import './App.css'
+import { DiVim } from 'react-icons/di';
 
-/* const[categorier, setCategorier] = useState([""]); */
- 
-const FilterFunction = ({...recipes}: RecipeInterface[]): JSX.Element  => {
+function App() {
+    const [value, setValue] = useState()
+    const onChange = async (e) => {
+        setValue(e.target.value)
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts')
     
-async function getRecipesByCategory() {
-    const response = await axios.get(`${API_URL}/categories`)
-
-
-    if (response.status===200){
-        const categories = response.data
-        console.log(categories)
-        const filter = categories.filter((filtrera:any) => filtrera.name === "kött") 
-        console.log(filter)
     }
-   
-   
-    /* setCategorier(categories) */
-        
-
-    
-}
-
-function getSelectedCategory() {
-   // let categoryVariable = getRecipesByCategory()
-    /* const Filter = categorier.filter((filtrera) => filtrera.name === "Kött") */
-   /*  console.log (Filter) */
-
-    
-
-}
-   //Get all categories from API
-
-   //Get all recipes by category from API
 
     return (
-        <div>
-            <input
-             placeholder="search..."
-             type="text" />
-            <button onClick={getRecipesByCategory}>Hämta categorier</button>
-            <button onClick={getSelectedCategory}>Filtrera</button>
-            {/* Rendera recept eller filtrerade recept här */}
-        </div>
-    );
-};
+        <div className="App">
+            <div className="search">
+                <div>
+                    <input type="text" onChange={onChange} value={value} />
+                    <button>search</button>
+                </div>
+                <div className='dropdown-content'>
+                    {
+                        value &&
+                        data.filter(item => item.title.startsWith(value) && item.tilte !== value)
+                        .slice(0, 5)
+                        .map(item => <div key={item.id} onClick={(e) => setValue(item.title)}>
+                            {item.tilte} <hr />
+                            </div>)
+                    }
 
-export default FilterFunction;
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
