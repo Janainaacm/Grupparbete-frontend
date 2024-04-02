042e2317cfd3a2874f7974575e750bdf8b48cecf
Jonas Ullvahn <ullvahn@gmail.com>
2024-04-02 17:14:35 +0200

    add fetchCategoryByID and clearDB

--- vite-project/src/store/APIState.ts
+++ vite-project/src/store/APIState.ts
@@ -17,21 +17,22 @@ interface APIState {
   fetchRecipeList: () => Promise<void>;
   postRecipe: (newRecipe: RecipeInterface) => Promise<number>;
   deleteRecipe: (recipeId: string) => Promise<void>;
+  fetchCategories: () => Promise<void>;
+  fetchCategoryByID: (categoryName: string) => Promise<RecipeInterface[]>;
+  clearDB: (key: string) => Promise<void>;
 }
 
 // skapar global state och fyller 'recipes' med samtliga recept i databasen.
 export const useAPIState = create<APIState>((set) => ({
-  
   recipeList: [],
   allCategories: [],
-  
 
   fetchRecipeList: async () => {
     try {
       const response = await axios.get(`${API_URL}/recipes/`);
       if (response.status === 200) {
-        set({ 
-          recipeList: response.data 
+        set({
+          recipeList: response.data,
         });
         console.log(response.data);
       }
@@ -63,7 +64,7 @@ export const useAPIState = create<APIState>((set) => ({
     try {
       const response = await axios.get(`${API_URL}/recipes/${recipeID}`);
       if (response.status === 200) {
-        console.log("Successfully loaded recipe");
+        console.log("Success loading recipe");
         console.log(response.data);
         return response.data;
       }
@@ -77,7 +78,7 @@ export const useAPIState = create<APIState>((set) => ({
       const response = await axios.delete(`${API_URL}/recipes/${recipeId}`);
 
       if (response.status === 200) {
-        console.log("successfully deleted ID: ", recipeId );
+        console.log("successfully deleted ID: ", recipeId);
         set((state) => ({
           recipeList: state.recipeList.filter(
             (recipe) => recipe._id !== recipeId
@@ -89,16 +90,14 @@ export const useAPIState = create<APIState>((set) => ({
     }
   },
 
-
   // NICK
   //POST - /recipes/{recipeId}/ratings - Lägger till ett omdöme för ett recept
-  
-  //GET - /recipes/{recipeId}/comments - Hämtar alla kommentarer för ett recept
-  
-  //POST - /recipes/{recipeId}/comments - Lägger till en kommentar för ett recept
-  
 
-  //JONAS 
+  //GET - /recipes/{recipeId}/comments - Hämtar alla kommentarer för ett recept
+
+  //POST - /recipes/{recipeId}/comments - Lägger till en kommentar för ett recept
+
+  //JONAS
   //GET - /categories - Hämtar alla kategorier
 
   fetchCategories: async () => {
@@ -107,17 +106,50 @@ export const useAPIState = create<APIState>((set) => ({
       if (response.status === 200) {
         console.log("success fetching categories");
         set({
-          allCategories: response.data
+          allCategories: response.data,
         });
         console.log(response.data);
       }
     } catch (error) {
-      console.log("error", error);
+      console.log("error fetching categories", error);
     }
   },
 
   //GET - /categories/{categoryName}/recipes - Hämtar alla recept i en viss kategori
-  
+
+  fetchCategoryByID: async (categoryName: string) => {
+    try {
+      const response = await axios.get(
+        `${API_URL}/categories/${categoryName}/recipes`
+      );
+      if (response.status === 200) {
+        console.log("success fetching category by ID");
+        console.log(response.data);
+        return response.data;
+      }
+    } catch (error) {
+      console.log("error fetching category by ID", error);
+    }
+  },
+
   //GET - /clear - Tömmer api:et på all data
-  
+
+  clearDB: async (key: string) => {
+    if (key === "ClearThisShitHole") {
+      try {
+        const response = await axios.get(`${API_URL}/clear`);
+        if (response.status === 200) {
+          console.log("successfully cleared database");
+          set({
+            recipeList: [],
+          });
+        }
+      } catch (error) {
+        console.log("error clearing database", error);
+      }
+    } else {
+      console.log("Requesting deletion failed!");
+      console.log("Contact Global-State support!");
+    }
+  },
 }));
