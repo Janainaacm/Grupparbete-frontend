d03c7e379fa6fd66ed3bf5e7479789afae75b74b
Janainaacm <janaina.maneiro@gmail.com>
2024-04-05 10:52:52 +0200

    recipePage basic layout

--- vite-project/src/pages/recipes/RecipePage.tsx
+++ vite-project/src/pages/recipes/RecipePage.tsx
@@ -1,39 +1,44 @@
-import { useEffect } from 'react';
-import NavBar from '../../globalComponents/NavBar';
-import Footer from "../../globalComponents/Footer.tsx";
-import DisplayRecipes from '../../globalComponents/DisplayRecipes';
-import { useAPIState } from '../../store/APIState';
-import { useLocation } from 'react-router-dom';
-import { RecipeInterface } from '../../Types';
-import FilterComponent from '../../globalComponents/FilterComponent';
+import { useEffect } from "react";
+import NavBar from "../../globalComponents/NavBar";
+import DisplayRecipes from "../../globalComponents/DisplayRecipes";
+import { useAPIState } from "../../store/APIState";
+import { useLocation } from "react-router-dom";
+import { RecipeInterface } from "../../Types";
+import FilterComponent from "../../globalComponents/FilterComponent";
+import Card from "./Card";
+import "./RecipePage.css";
 
 const RecipePage = (): JSX.Element => {
- const location = useLocation();
-  const { recipeList } = useAPIState();
+  const location = useLocation();
+  const { recipeList, fetchRecipeList } = useAPIState();
+
+  useEffect(() => {
+    fetchRecipeList();
+  }, []);
 
   let showRecipes = location.state as RecipeInterface[];
- if (!showRecipes){
-   showRecipes = recipeList;
- }
+  if (!showRecipes) {
+    showRecipes = recipeList;
+  }
 
- console.log(recipeList, "tydligen alla recept")
- console.log(showRecipes, "det vi skickar")
-  
+  console.log(recipeList, "tydligen alla recept");
+  console.log(showRecipes, "det vi skickar");
 
   return (
-    <>
+    <div>
       <NavBar />
-      <div>
-
-        {/* {showAll()} {} */}
-        <FilterComponent></FilterComponent>
-
-        <DisplayRecipes recipeList={recipeList} showDeleteButton={false} />
-
-      </div>
+      <main>
+        {showRecipes.map((item) => {
+          return <Card item={item} key={item._id} />;
+        })}
+      </main>
+  
+      {/* <FilterComponent></FilterComponent> */}
+  
+      <DisplayRecipes recipeList={recipeList} showDeleteButton={false} />
+  
       <Footer/>
-    </>
+    </div>
   );
-};
 
 export default RecipePage;
