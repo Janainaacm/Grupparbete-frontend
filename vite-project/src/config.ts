b3269875c6fddbed5498802cb1bcc2f5aa4e4805
Jonas Ullvahn <ullvahn@gmail.com>
2024-04-11 12:28:15 +0200

    add cocktail Global State

--- vite-project/src/config.ts
+++ vite-project/src/config.ts
@@ -1,3 +1,7 @@
 export const API_URL = "https://sti-java-grupp5-wjfjet.reky.se";
-const API_URL2 = "https://sti-java-grupp2-afmbgd.reky.se";
-const API_URL3 = "https://sti-java-grupp1-5znlnp.reky.se"; 
+export const COCKTAIL_API_URL = "https://www.thecocktaildb.com/api/json/v1/1"
+
+
+const API_URL2 = "https://sti-java-grupp2-afmbgd.reky.se";  // grupp 2
+const API_URL3 = "https://sti-java-grupp1-5znlnp.reky.se";  // grupp 1
+
